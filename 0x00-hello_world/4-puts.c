771f7bd1b44cb92626b55a047e691d96aea41e55
Mohammed Karoud <m.karoud@outlook.com>
2023-06-15 16:16:20 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/* print a stiring using puts function */
 int main(void)
 {
-	char str[] = '"Programming is like building a multilingual puzzle';
-
+	/* 
+	 * print a stiring using puts function 
+	 */
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 
 	return (0);
