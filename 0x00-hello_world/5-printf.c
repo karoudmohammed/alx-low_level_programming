f42ed1eb144af94b28cf5353f9f18adbbfb40528
Mohammed Karoud <m.karoud@outlook.com>
2023-06-15 16:27:10 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string using printf function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+
+	return (0);
+}
