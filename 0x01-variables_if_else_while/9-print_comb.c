604437aec3dbcc359b8eb464f237559256350187
Mohammed Karoud <m.karoud@outlook.com>
2023-06-16 20:00:37 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print only digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int s;
+
+for (s = 0; s < 128; s++)
+	if (isdigit(s))
+		putchar(s);
+		putchar(44);
+		putchar(32);
+
+putchar('\n');
+
+return (0);
+}
