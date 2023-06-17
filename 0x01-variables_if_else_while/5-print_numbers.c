49bfba3bc08302dbf9355478c6601eb81c96e677
Mohammed Karoud <m.karoud@outlook.com>
2023-06-16 19:10:57 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int s;
+
+for (s = 0; s < 10; s++)
+	printf("%d", s);
+
+printf("\n");
+
+return (0);
+}
