e2ad90b9148c990ac56832a7b4bd437b8b49792c
Mohammed Karoud <m.karoud@outlook.com>
2023-06-16 19:04:38 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the alphapet except q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+for (a = 97; a < 123; a++)
+{
+	if (a == 101 || a == 113)
+		continue;
+	putchar(a);
+	putchar(10);
+}
+
+return (0);
+}
