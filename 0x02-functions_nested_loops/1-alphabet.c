b98c852aa3f666d9f5ac95c93afe1574bdeb90b8
Mohammed Karoud <m.karoud@outlook.com>
2023-06-20 18:14:58 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+print_alphabet();
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,2 @@
-#include <unistd.h>
-
-int _putchar(char c):
+int _putchar(char c);
+void print_alphabet(void);
