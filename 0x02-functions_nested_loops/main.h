8a2c81afabd16d188310cbb9109deb0cf8ca4e8f
Mohammed Karoud <m.karoud@outlook.com>
2023-06-20 18:33:55 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,18 @@
-int _putchar(char c);
-void print_alphabet(void);
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void print_alphabet(void)
+{
+char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+unsigned int i;
+
+for (i = 0; i < strlen(alpha); i++)
+{
+	_putchar(alpha[i]);
+}
+}
