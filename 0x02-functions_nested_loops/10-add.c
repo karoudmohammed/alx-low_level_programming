b19554d90a3ca4330fd83758f7deeadd550a3686
Mohammed Karoud <m.karoud@outlook.com>
2023-06-21 19:14:18 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: number
+ * @b: number
+ *
+ * Description: 'add two integers and return the result'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
