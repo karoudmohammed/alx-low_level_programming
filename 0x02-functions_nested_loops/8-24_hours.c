f695da079d761b1f8f60c015592b0396f3d01bdd
Mohammed Karoud <m.karoud@outlook.com>
2023-06-21 18:43:03 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: 'print every minute of the day'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void format_minutes(int n)
+{
+int first_digit;
+int last_digit;
+
+if (n <= 9)
+{
+	_putchar('0');
+	_putchar('0' + n);
+}
+else
+{
+	first_digit = n / 10;
+	last_digit = n % 10;
+	_putchar('0' + first_digit);
+	_putchar('0' + last_digit);
+}
+}
+
+void jack_bauer(void)
+{
+int h, m;
+
+for (h = 0; h < 24; h++)
+{
+	for (m = 0; m < 60; m++)
+	{
+		format_minutes(h);
+		_putchar(':');
+		format_minutes(m);
+		_putchar('\n');
+	}
+}
+}
