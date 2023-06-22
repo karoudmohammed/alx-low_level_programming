68c7455c5228c59faa2772bc11785b4659d9ad9e
Mohammed Karoud <m.karoud@outlook.com>
2023-06-21 18:02:20 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,11 +14,6 @@ int print_last_digit(int n)
 {
 int last_digit;
 
-if (n < 0)
-{
-	n = n * (-1);
-}
-
 if (n == 0)
 {
 	_putchar('0' + n);
@@ -26,6 +21,10 @@ if (n == 0)
 }
 
 last_digit = n % 10;
+
+if (last_digit < 0)
+	last_digit *= (-1);
+
 _putchar('0' + last_digit);
 return (last_digit);
 }
