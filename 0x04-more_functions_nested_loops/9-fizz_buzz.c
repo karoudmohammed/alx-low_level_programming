3485dae3f31a972d5c4001fc29af253842998235
Mohammed Karoud <m.karoud@outlook.com>
2023-06-23 04:54:58 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * description: fizz-buzz test.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0 && i % 3 != 0)
+	{
+		printf("Buzz");
+	}
+	else if (i % 5 == 0 && i % 3 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else
+	{
+		printf("%d", i);
+	}
+
+	if (i != 1 || i != 100)
+	{
+		printf(" ");
+	}
+}
+
+printf("\n");
+return (0);
+}
