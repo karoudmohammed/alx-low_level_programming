594cd395eb3a8b1abf365b8aedaea6abc7f40051
Mohammed Karoud <m.karoud@outlook.com>
2023-06-29 18:50:55 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry function
+ *
+ * @s: a pointer to string
+ *
+ * description: function that changes lowercases to uppercases.
+ *
+ * Return: pointer
+ */
+
+char *string_toupper(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] = s[i] - 32;
+	}
+}
+return (s);
+}
