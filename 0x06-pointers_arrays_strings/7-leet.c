c6140b6508b301b4a3e4f3bf2cd0d1544cd9a5f7
Mohammed Karoud <m.karoud@outlook.com>
2023-06-29 19:44:16 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - entry function
+ *
+ * @s: a pointer to string
+ *
+ * description: function that encodes a string into 1337.
+ *
+ * Return: pointer
+ */
+
+char *leet(char *s)
+{
+int i, j;
+char *enc = "a7A7e3E3o0O0t7T7l1L1";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; enc[j] != '\0'; j++)
+	{
+		if (s[i] == enc[j])
+		{
+			s[i] = enc[j + 1];
+			break;
+		}
+	}
+}
+return (s);
+}
