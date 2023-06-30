29e71f2d8f86ab41c6e7257b5411f0ffa4262654
Mohammed Karoud <m.karoud@outlook.com>
2023-06-29 17:30:51 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - entry function
+ *
+ * @dest: a pointer to an destination
+ * @src: a pointer to a string
+ * @n: length
+ *
+ * description: function that copies a string.
+ *
+ * Return: a pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int c = 0;
+int i;
+
+while (src[c] != '\0')
+{
+	c++;
+}
+
+for (i = 0; i < n; i++)
+{
+	dest[i] = src[i];
+}
+
+return (dest);
+}
