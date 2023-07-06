dd87355a355ff142f20cdc9ecda5645717736103
Mohammed Karoud <m.karoud@outlook.com>
2023-07-05 17:29:50 -0700

    init, task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - entry function
+ *
+ * @s: a pointer to string
+ *
+ * description: function that prints a string, followed by new line
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+	_putchar('\n');
+	return;
+}
+
+_putchar(*s);
+s++;
+_puts_recursion(s);
+
+}
