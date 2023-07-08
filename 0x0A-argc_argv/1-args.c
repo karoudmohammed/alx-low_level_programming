c65b6fa7071f452c74621972b569964c1d74643d
Mohammed Karoud <m.karoud@outlook.com>
2023-07-07 17:44:47 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry function
+ *
+ * @argc: length of the array
+ * @argv: array of command line arguments
+ *
+ * description: main function.
+ *
+ * Return: Success 0
+ */
+
+int main(int argc, char *argv[])
+{
+argv[argc] = "";
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
