e8bc1ed29fad795beeb5b3de17c591c686cf0514
Mohammed Karoud <m.karoud@outlook.com>
2023-07-07 17:35:02 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry function
+ *
+ * @argv: array of command line arguments
+ * @argc: length of the array
+ *
+ * description: main function.
+ *
+ * Return: Success 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+
+return (0);
+}
