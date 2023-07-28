3569b3f720732d205d7a89d48dec8a0d7d5b2a79
Mohammed Karoud <m.karoud@outlook.com>
2023-07-28 02:23:53 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * first - entry function
+ *
+ * description: print string before main
+ *
+ * Return: Nothing
+ */
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
