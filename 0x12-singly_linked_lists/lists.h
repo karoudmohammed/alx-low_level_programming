d01b686f8fa4d262cdbb73d859a72c6e429e7d05
Mohammed Karoud <m.karoud@outlook.com>
2023-07-27 17:47:39 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
