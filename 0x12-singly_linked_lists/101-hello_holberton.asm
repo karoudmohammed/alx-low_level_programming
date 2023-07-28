45a0e446f516cd13461eec5516313ff73c6f959d
Mohammed Karoud <m.karoud@outlook.com>
2023-07-28 04:32:30 -0700

    task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-    hello: db "Hello, Holberton", 0
-    fmt: db "%s\n", 0
+    hello: db "Hello, Holberton", 0xa, 0
+    fmt: db "%s", 0
 section .text
     global main
     extern printf
