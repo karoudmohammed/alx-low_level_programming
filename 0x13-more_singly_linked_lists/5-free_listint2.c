9ab290db0d3cfb510486f70fa948e735e776489d
Mohammed Karoud <m.karoud@outlook.com>
2023-08-01 17:52:03 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - entry function
+ *
+ * @head: pointer to list node
+ *
+ * description: frees a listint_t list.
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *node;
+
+while (*head != NULL)
+{
+	node = (*head)->next;
+	free(*head);
+	*head = node;
+}
+
+*head = NULL;
+}
