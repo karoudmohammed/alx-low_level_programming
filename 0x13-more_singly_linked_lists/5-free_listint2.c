f6df207aad7d95ae7cd8ab8f8d5a9a40e9482f6e
Mohammed Karoud <m.karoud@outlook.com>
2023-08-02 17:33:36 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,16 +15,15 @@ void free_listint2(listint_t **head)
 {
 listint_t *node;
 
-if (head == NULL)
+if (head != NULL)
 {
-	return;
-}
-while (*head != NULL)
-{
-	node = (*head)->next;
-	free(*head);
-	*head = node;
-}
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
 
-*head = NULL;
+	*head = NULL;
+}
 }
