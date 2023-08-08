5f896cbb0670012198b443757c9c004f9d153d3f
Mohammed Karoud <m.karoud@outlook.com>
2023-08-08 09:10:14 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,14 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int w_bytes;
+int w_bytes, len = 0;
 
 if (filename == NULL)
 {
 	return (-1);
 }
 
-fd = open(filename, O_WRONLY | O_TRUNC);
+fd = open(filename, O_RDWR | O_TRUNC);
 if (fd == -1)
 {
 	fd = creat(filename, S_IREAD | S_IWRITE);
@@ -34,7 +34,11 @@ if (fd == -1)
 
 if (text_content)
 {
-	w_bytes = write(fd, text_content, sizeof(text_content) - 1);
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	w_bytes = write(fd, text_content, len);
 }
 else
 {
