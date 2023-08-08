a0d74a979ba4664c242b4f0732a49a47e8e4b39f
Mohammed Karoud <m.karoud@outlook.com>
2023-08-08 08:51:18 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - entry function
+ *
+ * @filename: path to file
+ * @text_content: text
+ *
+ * description: creates a file.
+ *
+ * Return: Integer
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int w_bytes;
+
+if (filename == NULL)
+{
+	return (-1);
+}
+
+fd = open(filename, O_RDWR | O_TRUNC);
+if (fd == -1)
+{
+	fd = creat(filename, S_IREAD | S_IWRITE);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+}
+
+if (text_content)
+{
+	w_bytes = write(fd, text_content, sizeof(text_content));
+}
+else
+{
+	w_bytes = write(fd, '\0', 1);
+}
+
+if (w_bytes == -1)
+{
+	close(fd);
+	return (-1);
+}
+
+close(fd);
+return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/file.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
